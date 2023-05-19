76682bf611b61c40f54c96ce353371c956c5accc
Yavin Torres <yavintorres@gmail.com>
2023-05-18 20:40:38 -0700

    correct and fetch images

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("running scripts")
+
+const renderCards = (infoArray) => {
+    const cardContainer = document.getElementById("card-container")
+    infoArray.forEach(cardInfo => {
+        const img = document.createElement("img")
+        img.src = cardInfo.img
+
+        cardContainer.appendChild(img)
+       
+    })
+}
+
+fetch("https://rickandmortyapi.com/api/character")
+.then(response => response.json())
+.then(info => renderCards(info.results))
